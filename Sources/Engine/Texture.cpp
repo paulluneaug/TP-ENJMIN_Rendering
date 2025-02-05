3102f25680d7be6f741c65bb8d8529b5f502a046
p.luneau <paul.luneau@mail.com>
2025-02-05 09:18:22 +0100

    push texture & cube correction
    (cherry picked from commit 1425927ca1461356210ab4aa2d817763dcfea3e9)

---
+++ Sources/Engine/Texture.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+
+#include "Texture.h"
+
+#include "DDSTextureLoader.h"
+
+using namespace DirectX;
+
+void Texture::Create(DeviceResources* deviceRes) {
+	auto d3dDevice = deviceRes->GetD3DDevice();
+	auto d3dContext = deviceRes->GetD3DDeviceContext();
+
+	CreateDDSTextureFromFileEx(d3dDevice,
+		(std::wstring(L"Textures/Compiled/") + textureName + L".dds").c_str(), 0,
+		D3D11_USAGE_DEFAULT, D3D11_BIND_SHADER_RESOURCE, 0, 0,
+		DDS_LOADER_IGNORE_SRGB,
+		nullptr, textureRV.ReleaseAndGetAddressOf());
+
+	CD3D11_SAMPLER_DESC sampDesc = CD3D11_SAMPLER_DESC(D3D11_DEFAULT);
+	sampDesc.Filter = D3D11_FILTER_MIN_MAG_POINT_MIP_LINEAR;
+	d3dDevice->CreateSamplerState(&sampDesc, &samplerState);
+}
+
+void Texture::Apply(DeviceResources* deviceRes) {
+	auto d3dContext = deviceRes->GetD3DDeviceContext();
+	d3dContext->PSSetShaderResources(0, 1, textureRV.GetAddressOf());
+	d3dContext->PSSetSamplers(0, 1, samplerState.GetAddressOf());
+}
