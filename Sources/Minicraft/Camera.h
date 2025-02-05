77ffe52ceb9e5855733716d0f722e83cd4f998fe
p.luneau <paul.luneau@mail.com>
2025-02-05 09:16:03 +0100

    push camera
    (cherry picked from commit 5f73b1e27acb4f61c7e65c3eb0fcb526104e5640)

---
+++ Sources/Minicraft/Camera.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Engine/Buffers.h"
+
+using namespace DirectX;
+using namespace DirectX::SimpleMath;
+
+class Camera {
+	float fov;
+	float nearPlane = 0.01f;
+	float farPlane = 500.0f;
+	
+	Vector3 camPos = Vector3(0, 0, 0);
+	Quaternion camRot = Quaternion();
+	Matrix projection;
+	Matrix view;
+
+	int lastMouseX = 0;
+	int lastMouseY = 0;
+
+	struct MatrixData {
+		Matrix mView;
+		Matrix mProjection;
+	};
+	ConstantBuffer<MatrixData>* cbCamera = nullptr;
+public:
+	Camera(float fov, float aspectRatio);
+	~Camera();
+
+	void UpdateAspectRatio(float aspectRatio);
+	void Update(float dt, DirectX::Keyboard::State kb, DirectX::Mouse* ms);
+
+	void ApplyCamera(DeviceResources* deviceRes);
+};
