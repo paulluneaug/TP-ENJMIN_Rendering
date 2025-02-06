b5bdc5dedc21d848525b61db3c066d9e39b5c30c
p.luneau <paul.luneau@mail.com>
2025-02-06 11:55:27 +0100

    More Vector3Int methods

--- Sources/Engine/Vector3Int.cpp
+++ Sources/Engine/Vector3Int.cpp
@@ -17,3 +17,23 @@ Vector3Int::Vector3Int(int x, int y, int z):
 	Z(z)
 {
 }
+
+Vector3 Vector3Int::ToVector3()
+{
+	return Vector3{ float(X), float(Y), float(Z) };
+}
+
+Vector3Int Vector3Int::operator+(const Vector3Int& v) const
+{
+	return Vector3Int(X + v.X, Y + v.Y, Z + v.Z);
+}
+
+bool Vector3Int::operator==(const Vector3Int& other) const
+{
+	return X == other.X && Y == other.Y && Z == other.Z;
+}
+
+Vector3Int Vector3Int::operator*(const int& scalar) const
+{
+	return Vector3Int(X * scalar, Y * scalar, Z * scalar);
+}
--- Sources/Engine/Vector3Int.h
+++ Sources/Engine/Vector3Int.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <SimpleMath.h>
+
+using namespace DirectX::SimpleMath;
 
 class Vector3Int 
 {
@@ -8,4 +11,18 @@ public:
 public:
 	Vector3Int();
 	Vector3Int(int x, int y, int z);
+
+	Vector3 ToVector3();
+
+	Vector3Int operator+ (const Vector3Int& v) const;
+	bool operator== (const Vector3Int& other) const;
+	Vector3Int operator* (const int& scalar) const;
+};
+
+struct Vector3IntHasher
+{
+	size_t operator() (const Vector3Int& v) const
+	{
+		return std::hash<int>()(v.X) ^ std::hash<int>()(v.Y) ^ std::hash<int>()(v.Z);
+	}
 };
